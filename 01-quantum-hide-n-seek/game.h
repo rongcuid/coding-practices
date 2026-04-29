9003d0274d4754d66c5d76ac205377dde6992409
Rongcui Dong <rongcuid@outlook.com>
2026-04-28 22:29:05 -0400

    Massive rename

--- 01-quantum-hide-n-seek/game.h
+++ 01-quantum-hide-n-seek/game.h
@@ -35,5 +35,5 @@ typedef struct {
 
 observation_result_t observe_hider(const hider_state_t *hider,
                                    const observation_t *observation_t,
-                                   const mscc_allocator_t *up,
-                                   mscc_arena_t scratch);
+                                   const msc_allocator_t *up,
+                                   msc_arena_t scratch);
